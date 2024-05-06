b7dd9c4f2fae8ce90236552b62d5489a5560be7d
Gilles <quodbach.gilles@gmail.com>
2024-05-06 11:36:14 +0200

    first commit

---
+++ src/main/java/fr/fms/apitrainings/ApiTrainingsApplication.java
@@ -0,0 +1,13 @@
+package fr.fms.apitrainings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTrainingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTrainingsApplication.class, args);
+	}
+
+}
