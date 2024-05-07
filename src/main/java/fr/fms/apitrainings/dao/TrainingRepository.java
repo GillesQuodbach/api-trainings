014c4e75c674e51660860473b094cbf7b5db3359
Gilles <quodbach.gilles@gmail.com>
2024-05-07 09:39:50 +0200

    MC p19 ok

--- src/main/java/fr/fms/apitrainings/dao/TrainingRepository.java
+++ src/main/java/fr/fms/apitrainings/dao/TrainingRepository.java
@@ -2,7 +2,12 @@ package fr.fms.apitrainings.dao;
 
 import fr.fms.apitrainings.entities.Training;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+import java.util.List;
 
 public interface TrainingRepository extends JpaRepository<Training, Long> {
+    public List<Training> findAll();
 }
--- src/main/java/fr/fms/apitrainings/web/TrainingController.java
+++ src/main/java/fr/fms/apitrainings/web/TrainingController.java
@@ -14,7 +14,7 @@ import java.util.Objects;
 import java.util.Optional;
 import java.net.URI;
 
-@CrossOrigin("*")
+@CrossOrigin
 //@CrossOrigin("http://localhost:4200")
 @RestController
 @RequestMapping("/api")
